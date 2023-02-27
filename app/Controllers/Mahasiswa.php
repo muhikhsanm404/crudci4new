869c96d75ea9d9c073a12c51210ce8384d377a76
iqbalthex <iqbalariem232@gmail.com>
2023-02-27 08:16:25 +0700

    refactor code (50%)

--- app/Controllers/Mahasiswa.php
+++ app/Controllers/Mahasiswa.php
@@ -1,97 +1,128 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\ModelMahasiswa;
+
 class Mahasiswa extends BaseController
 {
-    protected $modelMahasiswa;
-    
-    public function __construct()
-    {
-        $this->modelMahasiswa = new ModelMahasiswa();
-    }
-    public function index()
-    {
-        $data=[
-            'datamahasiswa'=>$this->modelMahasiswa->findAll()
-        ];
-        return view('tampil_mahasiswa',$data);
-    }
-    public function create()
-    {
-        
-        if($this->request->getPost()){
-            $fields=[
-                'nim'=>$this->request->getPost('nim'),
-                'nama'=>$this->request->getPost('nama'),
-                'email'=>$this->request->getPost('email'),
-                'jurusan'=>$this->request->getPost('jurusan'),
-                'alamat'=>$this->request->getPost('alamat')
-                // 'validation' => \Config\Services::validation()
-            ];
-            $this->modelMahasiswa->insert($fields);
-            session()->setFlashdata('pesan','Selamat Kak!, Data berhasil ditambahkan.'); 
-            return redirect()->to('');
+  protected $modelMahasiswa;
+  protected $pesan;
 
-                                 //validasi input
-                                 if (!$this->validate([
-                                    'nim' => [
-                                        'rules' => 'required|is_unique[number.nim]',
-                                        'errors' => [
-                                            'required' => '{field} data harus diisi',
-                                            'is_unique' => '{field} data sudah terdaftar'
-                                        ]
-                                        ]         
-                        
-                                 ])) 
 
-                                   {
-                                
-                                    //     dd($validation);
-                                    $validation = \Config\Services::validation();
-                                   return redirect()->to('/tambah')->withInput()->with('validation',$validation);
-                                   }
-        
-    
+  public function __construct()
+  {
+    $this->modelMahasiswa = new ModelMahasiswa();
+  }
 
-        }
-        // session();
-        $data=[
-            'title'=> 'Form Tambah Data Mahasiswa',
-            'validation' => \Config\Services::validation()
-        ];
-       
-        return view('tambah_mahasiswa',$data);
-   
 
+  public function index()
+  {
+    $data = [
+      'mahasiswa' => $this->modelMahasiswa->findAll()
+    ];
+    if ($pesan = session()->getFlashdata('pesan')) {
+      $data['pesan'] = $pesan;
     }
-    public function edit()
-    {
-        $nim = $this->request->uri->getSegment(2);
-        $hasil = $this->modelMahasiswa->find($nim);
-        $data = [
-            'hasil' => $hasil
-        ];
-        return view('edit_mahasiswa',$data);
-         //dd($nim);
+    return view('tampil_mahasiswa', $data);
+  }
+
+
+  public function create()
+  {
+    $data = [
+      'title' => 'Form Tambah Data Mahasiswa',
+      'errors' => session()->getFlashdata('errors'),
+    ];
+
+    $data['isInvalid'] = function($field) use ($data) {
+      return isset($data['errors'][$field]) ? 'is-invalid' : '';
+    };
+
+    if ($pesan = session()->getFlashdata('pesan')) {
+      $data['pesan'] = $pesan;
     }
-    public function update()
-    {
-        $this->modelMahasiswa->set('nama', $this->request->getPost('nama'));
-        $this->modelMahasiswa->set('email', $this->request->getPost('email'));
-        $this->modelMahasiswa->set('jurusan', $this->request->getPost('jurusan'));
-        $this->modelMahasiswa->set('alamat', $this->request->getPost('alamat'));
-        $this->modelMahasiswa->where('nim', $this->request->getPost('nim'));
-        $this->modelMahasiswa->update();
-        session()->setFlashdata('pesan','Selamat Kak!, Data berhasil diedit.'); 
-        return redirect()->to('/');
+    return view('tambah_mahasiswa', $data);
+  }
+
+
+  public function save()
+  {
+    $valid = $this->validate([
+      'nama' => [
+        'rules' => 'required|alpha',
+        'errors' => [
+          'required' => 'Nama harus diisi!',
+          'alpha' => 'Nama hanya boleh mengandung alfabet!',
+        ],
+      ],
+      'nim' => [
+        'rules' => 'required|is_unique[mahasiswa.nim]',
+        'errors' => [
+          'required' => 'NIM harus diisi!',
+          'is_unique' => 'NIM sudah terdaftar!',
+        ],
+      ],
+    ]);
+
+    if (!$valid) {;
+      $this->setPesan('danger', 'Maaf Kak!, Data gagal ditambahkan.');
+      return redirect()->to(base_url('tambah'))->withInput()
+        ->with('pesan', $this->pesan)
+        ->with('errors', $this->validator->getErrors());
     }
 
-    public function delete()
-    {
-        $nim = $this->request->uri->getSegment(2);
-        $this->modelMahasiswa->delete(['nim' => $nim]);
-        session()->setFlashdata('pesan','Selamat Kak!, Data berhasil dihapus.'); 
-        return redirect()->to('/');
+    $fields = [
+      'nim'  => $this->request->getPost('nim'),
+      'nama' => $this->request->getPost('nama'),
+      'email'   => $this->request->getPost('email'),
+      'jurusan' => $this->request->getPost('jurusan'),
+      'alamat'  => $this->request->getPost('alamat'),
+    ];
+
+    if ($this->modelMahasiswa->insert($fields)) {
+      $this->setPesan('success', 'Selamat Kak!, Data berhasil ditambahkan.');
+    } else {
+      $this->setPesan('danger', 'Maaf Kak!, Data gagal ditambahkan.');
     }
+    return redirect()->to('')
+      ->withInput()
+      ->with('pesan', $this->pesan);
+  }
+
+
+  public function edit()
+  {
+      $nim = $this->request->uri->getSegment(2);
+      $hasil = $this->modelMahasiswa->find($nim);
+      $data = [
+          'hasil' => $hasil
+      ];
+      return view('edit_mahasiswa',$data);
+       //dd($nim);
+  }
+  public function update()
+  {
+      $this->modelMahasiswa->set('nama', $this->request->getPost('nama'));
+      $this->modelMahasiswa->set('email', $this->request->getPost('email'));
+      $this->modelMahasiswa->set('jurusan', $this->request->getPost('jurusan'));
+      $this->modelMahasiswa->set('alamat', $this->request->getPost('alamat'));
+      $this->modelMahasiswa->where('nim', $this->request->getPost('nim'));
+      $this->modelMahasiswa->update();
+      session()->setFlashdata('pesan','Selamat Kak!, Data berhasil diedit.'); 
+      return redirect()->to('/');
+  }
+
+  public function delete()
+  {
+      $nim = $this->request->uri->getSegment(2);
+      $this->modelMahasiswa->delete(['nim' => $nim]);
+      session()->setFlashdata('pesan','Selamat Kak!, Data berhasil dihapus.'); 
+      return redirect()->to('/');
+  }
+
+
+  public function setPesan($tipe, $msg) {
+    $this->pesan = compact('tipe', 'msg');
+  }
 }
